9c61a562ca33c51153660e63ef12f58ca47423fb
Steve Fan <sf200212345@gmail.com>
2023-03-24 06:43:16 -0400

    text shows up in chat window

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+
+  // in format [text, user] where user can be true or false
+  const [chatText, setChatText] = useState([]);
+  const [port, setPort] = useState(3000);
+
+  // adds the current input to setChatText and makes a request to the API
+  const submitText = currText => {
+    setChatText([...chatText, [currText, true]]);
+    console.log(chatText);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='chat-container'>
+      <div className='chat-window'>
+        { chatText.map((curr, index) => (curr[1] ? <div key={index} className='user text'>{curr[0]}</div> : <div key={index} className='not-user text'>{curr[0]}</div>)) }
+      </div>
+      <div className='input-window'>
+        <input type="text" onKeyDown={(e) => {if (e.key === "Enter") {submitText(e.target.value);e.target.value = "";}}} />
+      </div>
     </div>
   );
 }
